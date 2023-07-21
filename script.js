d003eda6caecf0746af9786163bd96ef3f4a66f1
BabyStu <srw33de@gmail.com>
2023-07-20 19:26:33 -0500

    javascript updates

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const youtubeApi = 'AIzaSyAHvcBl8WthZkECx9RMvPLuU6tU3c7I0XM';
 
 var searchBarInput = document.querySelector('#searchBar');
 var imdbTitleBox = document.querySelector('#titlebox');
+var imdbInfoBox = document.querySelector('#imdbInfo');
 
 function imdbSearch(searchInput) {
     var locQueryUrl = 'http://www.omdbapi.com/?';
@@ -17,18 +18,13 @@ function imdbSearch(searchInput) {
         })
         .then(function (imdbData) {
 
-            console.log(imdbData);
-
+            console.log(imdbData)
 
             var title = imdbData.Title;
-            var metascore = imdbData.Metascore;
-            var imdbRating = imdbData.imdbRating;
-
-            console.log(title);
-            console.log(metascore);
-            console.log(imdbRating);
+            var array = [title, imdbData.Plot, imdbData.Director, imdbData.Actors, imdbData.Year, imdbData.BoxOffice, imdbData.Ratings[1]];
 
             youtubeSearch(title);
+            printImdb(array);
         })
 }
 
@@ -47,18 +43,39 @@ function youtubeSearch (movieTitle) {
             console.log(video)
 
             var videoId = video.items[0].id.videoId;
-            var iframe = document.createElement("iframe");
-            iframe.src = "https://www.youtube.com/embed/" + videoId;
-            iframe.width = "640";
-            iframe.height = "360";
+            var iframe = document.createElement('iframe');
+            iframe.src = 'https://www.youtube.com/embed/' + videoId;
+            iframe.width = '640';
+            iframe.height = '360';
 
-            var container = document.getElementById("videoContainer");
-            container.innerHTML = "";
-            container.appendChild(iframe);
+            var trailerVideo = document.getElementById('video');
+            trailerVideo.innerHTML = '';
+            trailerVideo.appendChild(iframe);
 
         })
 }
 
+function printImdb(data) {
+    var divBox = document.createElement('div');
+    var titleEl = document.createElement('h1');
+    var plotEl = document.createElement('p');
+    var directorActorEl = document.createElement('p');
+    var releaseEl = document.createElement('p');
+    var ratingsEl = document.createElement('p');
+
+    console.log(data)
+
+    titleEl.innerHTML = data[0];
+    plotEl.innerHTML = data[1];
+    directorActorEl.innerHTML = 'Directed by ' + data[2] + ', the cast consists of ' + data[3];
+    releaseEl.innerHTML = 'This was released in ' + data[4] + ' and made ' + data[5] + ' in box office release.';
+    ratingsEl.innerHTML = data[6].Source + ' rated this ' + data[6].Value;
+
+    divBox.append(titleEl, plotEl, directorActorEl, releaseEl, ratingsEl);
+
+    imdbInfoBox.innerHTML = '';
+    imdbInfoBox.append(divBox);
+}
 
 
 function searchSubmit(event) {
